5aa5b6a1e81e4067b548e364b88851b88d80ef85
Arik Chakma <arikchangma@gmail.com>
2024-04-27 04:14:01 +0600

    feat: add redis

---
+++ src/lib/redis.ts
@@ -0,0 +1,49 @@
+import { createClient } from 'redis';
+import { serverConfig } from './config';
+import { logError, logInfo, logWarning } from './logger';
+
+let cachedRedisClient: ReturnType<typeof createClient> | null = null;
+
+export async function connectRedis(): Promise<ReturnType<typeof createClient>> {
+  if (cachedRedisClient) {
+    logInfo('[redis] using cached client');
+    return cachedRedisClient;
+  }
+
+  cachedRedisClient = createClient({
+    url: serverConfig.redis.url,
+  });
+
+  try {
+    cachedRedisClient.on('ready', () => {
+      logInfo('[redis] ready');
+    });
+
+    cachedRedisClient.on('error', (err: any) => {
+      logError('[redis] error', err);
+    });
+
+    cachedRedisClient.on('reconnected', () => {
+      logInfo('[redis] reconnected');
+    });
+
+    cachedRedisClient.on('reconnectFailed', () => {
+      logError('[redis] reconnectFailed');
+    });
+
+    cachedRedisClient.on('disconnected', () => {
+      logWarning('[redis] disconnected');
+    });
+
+    logInfo('[redis] connecting to redis');
+    await cachedRedisClient.connect();
+    logInfo('[redis] connected to redis');
+
+    return cachedRedisClient;
+  } catch (error) {
+    logError('[redis] error connecting to redis', error);
+    await cachedRedisClient.quit();
+    cachedRedisClient = null;
+    throw error;
+  }
+}
