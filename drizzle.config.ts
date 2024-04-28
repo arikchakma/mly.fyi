0b379c815388da6f346c3d17db04e1948af8ff7d
Arik Chakma <arikchangma@gmail.com>
2024-04-28 06:08:45 +0600

    fix: missing database url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,16 @@
 import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is missing');
+}
+
 export default defineConfig({
   verbose: true,
   schema: './src/db/schema/index.ts',
   out: './drizzle',
   driver: 'better-sqlite',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 });
