6a261e4b065451099bb0ef5981405e2d652a61dd
Arik Chakma <arikchangma@gmail.com>
2024-05-06 05:45:23 +0600

    feat: add license and readme

--- src/components/ProjectEmails/ListProjectEmails.tsx
+++ src/components/ProjectEmails/ListProjectEmails.tsx
@@ -2,7 +2,7 @@ import { httpGet } from '@/lib/http';
 import type { ListProjectEmailsResponse } from '@/pages/api/v1/projects/[projectId]/emails/index';
 import { queryClient } from '@/utils/query-client';
 import { useQuery } from '@tanstack/react-query';
-import { Mail } from 'lucide-react';
+import { ArrowUpRight, Mail } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { EmptyItems } from '../EmptyItems';
 import { PageError } from '../Errors/PageError';
@@ -63,8 +63,7 @@ export function ListProjectEmails(props: ListProjectEmailsProps) {
           title='No emails found.'
           description='You can view the status of each email and track the delivery status. Start by sending an email.'
           linkText='Start Sending Email'
-          // TODO: Add the docs link here
-          // link={`/projects/${projectId}/emails/new`}
+          link='https://mly.fyi/docs/send-email'
           icon={Mail}
         />
       )}
@@ -73,6 +72,16 @@ export function ListProjectEmails(props: ListProjectEmailsProps) {
         <>
           <div className='flex items-center justify-between'>
             <h2 className='text-xl font-semibold'>Emails</h2>
+
+            <a
+              href='https://mly.fyi/docs/send-email'
+              className='rounded-md bg-zinc-800 px-2 py-1 text-sm text-zinc-50 shrink-0 mt-4'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <ArrowUpRight className='w-4 h-4 inline-block mr-1' />
+              Learn more
+            </a>
           </div>
           <div className='mt-6'>
             <div className='space-y-4'>
